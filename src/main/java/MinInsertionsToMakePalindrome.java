e891fb0053963883140e9088822a7cf406f28604
Yogesh <yogesh.bh@media.net>
2018-03-26 02:14:30 +0530

    Min insertions to make string palindrome

---
+++ src/main/java/MinInsertionsToMakePalindrome.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+class Solution {
+    private Map<Integer, Map<Integer, Integer>> dp = new HashMap();
+
+    public int solve(String A) {
+        return recurse(A, 0, A.length() - 1);
+    }
+
+    public int recurse(String str, int i, int j) {
+        if (i >= j)
+            return 0;
+        if (dp.containsKey(i) && dp.get(i).containsKey(j))
+            return dp.get(i).get(j);
+        char ch1 = str.charAt(i), ch2 = str.charAt(j);
+        int ans;
+        if (ch1 == ch2)
+            ans = recurse(str, i + 1, j - 1);
+        else {
+            int temp1 = recurse(str, i + 1, j);
+            int temp2 = recurse(str, i, j - 1);
+            ans = Math.min(temp1, temp2) + 1;
+        }
+        dp.putIfAbsent(i, new HashMap());
+        dp.get(i).put(j, ans);
+        //System.out.println(dp);
+        return ans;
+    }
+}
