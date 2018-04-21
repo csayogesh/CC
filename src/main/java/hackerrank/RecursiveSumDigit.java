327c26467c6376d7e7858b37da4d8278fa628019
Yogesh <ybhosale12@gmail.com>
2018-04-22 00:35:24 +0530

    1. Recursive sum digit

---
+++ src/main/java/hackerrank/RecursiveSumDigit.java
@@ -0,0 +1,28 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * Created by yogesh.bh on 22/04/18.
+ */
+public class RecursiveSumDigit {
+    static int digitSum(String n, int k) {
+        long sum = 0;
+        for (int i = 0; i < n.length(); i++) {
+            sum += n.charAt(i) - '0';
+        }
+        String val = String.valueOf(k * sum);
+        if (val.length() > 1)
+            return digitSum(val, 1);
+        return (int) (k * sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String n = in.next();
+        int k = in.nextInt();
+        int result = digitSum(n, k);
+        System.out.println(result);
+        in.close();
+    }
+}
