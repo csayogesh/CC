494b5c0d208f1afb3e64ce90f5bc6fdea2664e90
Yogesh <ybhosale12@gmail.com>
2018-04-21 12:49:44 +0530

    1. Story of the tree

--- src/main/java/hackerrank/StoryOfATree.java
+++ src/main/java/hackerrank/StoryOfATree.java
@@ -7,17 +7,6 @@ import java.util.*;
  * Created by yogesh.bh on 16/04/18.
  */
 public class StoryOfATree {
-
-    static class Node {
-        public int val;
-        public List<Node> children = new ArrayList<>();
-        public Node parent;
-
-        public Node(int i) {
-            val = i;
-        }
-    }
-
     static String storyOfATree(int n, int[][] edges, int k, int[][] guesses) {
         Map<Integer, Set<Integer>> adj = new HashMap<>();
         for (int[] edge : edges) {
@@ -26,22 +15,18 @@ public class StoryOfATree {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-        Map<Integer, Node> nodeMap = new HashMap<>();
-        makeTree(adj, 1, nodeMap);
+        Map<Integer, Set<Integer>> guessAdj = new HashMap<>();
+        for (int[] guess : guesses) {
+            guessAdj.putIfAbsent(guess[0], new LinkedHashSet<>());
+            guessAdj.get(guess[0]).add(guess[1]);
+        }
+        Map<Integer, Integer> counts = new HashMap<>();
+        dfsSubTree(1, 1, adj, guessAdj, counts);
+        dfsFull(1, 1, adj, guessAdj, counts);
+
         int success = 0;
         for (int hid = 1; hid <= n; hid++) {
-            if (hid != 1) {
-                makeRoot(nodeMap.get(hid));
-                nodeMap.get(hid).parent = null;
-            }
-            int correct = 0;
-            for (int[] guess : guesses)
-                try {
-                    if (nodeMap.get(guess[1]).parent != null && nodeMap.get(guess[1]).parent.val == guess[0])
-                        correct++;
-                } catch (Exception e) {
-                    System.out.println(1);
-                }
+            int correct = counts.getOrDefault(hid, 0);
             if (correct >= k)
                 success++;
         }
@@ -51,40 +36,34 @@ public class StoryOfATree {
         return success + "/" + n;
     }
 
+    private static void dfsFull(int node, int parent, Map<Integer, Set<Integer>> adjList, Map<Integer, Set<Integer>> guesses, Map<Integer, Integer> counts) {
+        counts.put(node, counts.getOrDefault(parent, 0));
+        if (guesses.containsKey(parent) && guesses.get(parent).contains(node))
+            counts.put(node, counts.get(node) - 1);
+        if (guesses.containsKey(node) && guesses.get(node).contains(parent))
+            counts.put(node, counts.get(node) + 1);
+        for (int child : adjList.get(node))
+            if (child != parent) {
+                dfsFull(child, node, adjList, guesses, counts);
+            }
+    }
+
+    private static void dfsSubTree(int node, int parent, Map<Integer, Set<Integer>> adjList, Map<Integer, Set<Integer>> guesses, Map<Integer, Integer> counts) {
+        for (int child : adjList.get(node))
+            if (child != parent) {
+                if (guesses.containsKey(node) && guesses.get(node).contains(child))
+                    counts.put(node, counts.getOrDefault(node, 0) + 1);
+                dfsSubTree(child, node, adjList, guesses, counts);
+                counts.put(node, counts.getOrDefault(node, 0) + counts.getOrDefault(child, 0));
+            }
+    }
+
     private static int gcdF(int a, int b) {
         if (b == 0)
             return a;
         if (b > a)
             return gcdF(b, a);
         return gcdF(b, a % b);
-    }
-
-    private static void makeRoot(Node root) {
-        if (root == null)
-            return;
-        Node oldParent = root.parent;
-        makeRoot(oldParent);
-        if (oldParent != null)
-            oldParent.parent = root;
-    }
-
-    private static Node makeTree(Map<Integer, Set<Integer>> adj, int root, Map<Integer, Node> map) {
-        if (adj.get(root).size() == 0) {
-            Node node = new Node(root);
-            map.put(root, node);
-            return node;
-        }
-        Node rNode = new Node(root);
-
-        List<Integer> ls = new ArrayList<>(adj.get(root));
-        for (int child : ls) {
-            adj.get(child).remove(root);
-            Node node = makeTree(adj, child, map);
-            rNode.children.add(node);
-            node.parent = rNode;
-        }
-        map.put(root, rNode);
-        return rNode;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
