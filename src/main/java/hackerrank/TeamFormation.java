4adc1615a17c0c01dd20ffb875d410ca89f94d15
Yogesh <ybhosale12@gmail.com>
2018-04-22 20:27:26 +0530

    1. Efficiency improved

--- src/main/java/hackerrank/TeamFormation.java
+++ src/main/java/hackerrank/TeamFormation.java
@@ -1,19 +1,24 @@
 package hackerrank;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
  * Created by yogesh.bh on 22/04/18.
  */
 public class TeamFormation {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer line = new StringTokenizer(sc.readLine(), " ");
+        int t = Integer.parseInt(line.nextToken());
         for (int i = 0; i < t; i++) {
-            int n = sc.nextInt();
+            line = new StringTokenizer(sc.readLine(), " ");
+            int n = Integer.parseInt(line.nextToken());
             List<Integer> ls = new ArrayList<>();
             for (int j = 0; j < n; j++)
-                ls.add(sc.nextInt());
+                ls.add(Integer.parseInt(line.nextToken()));
             int minSize = minTeamSize(ls);
             System.out.println(minSize);
         }
@@ -22,8 +27,7 @@ public class TeamFormation {
     private static int minTeamSize(List<Integer> ls) {
         if (ls.size() == 0)
             return 0;
-        Collections.sort(ls);
-        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         for (int elem : ls) {
             map.putIfAbsent(elem, 0);
             map.put(elem, map.get(elem) + 1);
