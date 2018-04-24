e178bc145be86ef8c06f26573a9c9dcbb961c91b
Yogesh <ybhosale12@gmail.com>
2018-04-24 11:53:46 +0530

    1. Quartiles simplified

--- src/main/java/hackerrank/Quartiles.java
+++ src/main/java/hackerrank/Quartiles.java
@@ -17,29 +17,28 @@ public class Quartiles {
             ls.add(sc.nextInt());
         }
         Collections.sort(ls);
-        double q2 = median(ls, 0, ls.size() - 1);
-        double q1, q3;
-        if (n % 2 == 1) {
-            q1 = median(ls, 0, -1 + n / 2);
-            q3 = median(ls, 1 + n / 2, n - 1);
-        } else {
-            q1 = median(ls, 0, (n-1) / 2);
-            q3 = median(ls, n / 2, n - 1);
-        }
+        double[] ar = median(ls, 0, ls.size() - 1);
+        double q2 = ar[0];
+        double q1 = median(ls, 0, (int) Math.floor(ar[1] - 0.1))[0];
+        double q3 = median(ls, 0, (int) Math.ceil(ar[1] + 0.1))[0];
         System.out.println(q1);
         System.out.println(q2);
         System.out.println(q3);
     }
 
-    private static double median(List<Integer> ls, int i, int j) {
+    private static double[] median(List<Integer> ls, int i, int j) {
         int n = j - i + 1;
-        double median = 0;
-        if (n % 2 == 1)
-            median = ls.get(i + n / 2);
-        else {
-            median += ls.get(i + n / 2);
-            median += ls.get(i + (n - 1) / 2);
-            median /= 2;
+        double[] median = new double[2];
+        if (n % 2 == 1) {
+            median[1] = i + n / 2;
+            median[0] = ls.get((int) median[1]);
+        } else {
+            median[1] = i + n / 2;
+            median[1] += i + (n - 1) / 2;
+            median[0] += ls.get(i + n / 2);
+            median[0] += ls.get(i + (n - 1) / 2);
+            median[0] /= 2;
+            median[1] /= 2;
         }
         return median;
     }
