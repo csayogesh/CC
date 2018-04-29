3a17f83025d13633f3d64287875c08bd3469189a
Yogesh <ybhosale12@gmail.com>
2018-04-29 16:07:43 +0530

    1. Min and max heap classes created

---
+++ src/main/java/hackerrank/MaxHeap.java
@@ -0,0 +1,90 @@
+package hackerrank;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yogesh.bh on 29/04/18.
+ */
+public class MaxHeap {
+    ArrayList<Integer> ls = new ArrayList<>();
+    int size = 0;
+
+    public MaxHeap() {
+    }
+
+    public MaxHeap(ArrayList<Integer> ls) {
+        this.ls = ls;
+        size = ls.size();
+        heapify();
+    }
+
+    public MaxHeap(int[] data) {
+        for (int x : data)
+            this.ls.add(x);
+        size = data.length;
+        heapify();
+    }
+
+    private void heapify() {
+        for (int i = ls.size() / 2; i >= 0; i--)
+            maxHeapify(i);
+    }
+
+    private void maxHeapify(int i) {
+        int l = left(i);
+        int r = right(i);
+        int largest;
+        if (l < size && ls.get(l) > ls.get(i))
+            largest = l;
+        else largest = i;
+        if (r < size && ls.get(r) > ls.get(largest))
+            largest = r;
+        if (largest != i) {
+            int tmp = ls.get(i);
+            ls.set(i, ls.get(largest));
+            ls.set(largest, tmp);
+            maxHeapify(largest);
+        }
+    }
+
+    int findMax() {
+        return ls.get(0);
+    }
+
+    int removeMax() {
+        int ans = findMax();
+        ls.set(0, ls.get(size - 1));
+        size = size - 1;
+        maxHeapify(0);
+        return ans;
+    }
+
+    void insert(int key) {
+        if (ls.size() > size) ls.set(size, Integer.MIN_VALUE);
+        else ls.add(Integer.MIN_VALUE);
+        size++;
+        increaseKey(size - 1, key);
+    }
+
+    private void increaseKey(int i, int key) {
+        ls.set(i, key);
+        while (i > 0 && ls.get(parent(i)) < ls.get(i)) {
+            int tmp = ls.get(parent(i));
+            ls.set(parent(i), ls.get(i));
+            ls.set(i, tmp);
+            i = parent(i);
+        }
+    }
+
+    int left(int i) {
+        return 2 * i;
+    }
+
+    int right(int i) {
+        return 2 * i + 1;
+    }
+
+    int parent(int i) {
+        return i / 2;
+    }
+}
---
+++ src/main/java/hackerrank/MinHeap.java
@@ -0,0 +1,90 @@
+package hackerrank;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yogesh.bh on 29/04/18.
+ */
+public class MinHeap {
+    ArrayList<Integer> ls = new ArrayList<>();
+    int size = 0;
+
+    public MinHeap() {
+    }
+
+    public MinHeap(ArrayList<Integer> ls) {
+        this.ls = ls;
+        size = ls.size();
+        heapify();
+    }
+
+    public MinHeap(int[] data) {
+        for (int x : data)
+            this.ls.add(x);
+        size = data.length;
+        heapify();
+    }
+
+    private void heapify() {
+        for (int i = ls.size() / 2; i >= 0; i--)
+            minHeapify(i);
+    }
+
+    private void minHeapify(int i) {
+        int l = left(i);
+        int r = right(i);
+        int min;
+        if (l < size && ls.get(l) < ls.get(i))
+            min = l;
+        else min = i;
+        if (r < size && ls.get(r) < ls.get(min))
+            min = r;
+        if (min != i) {
+            int tmp = ls.get(i);
+            ls.set(i, ls.get(min));
+            ls.set(min, tmp);
+            minHeapify(min);
+        }
+    }
+
+    int findMin() {
+        return ls.get(0);
+    }
+
+    int removeMax() {
+        int ans = findMin();
+        ls.set(0, ls.get(size - 1));
+        size = size - 1;
+        minHeapify(0);
+        return ans;
+    }
+
+    void insert(int key) {
+        if (ls.size() > size) ls.set(size, Integer.MAX_VALUE);
+        else ls.add(Integer.MAX_VALUE);
+        size++;
+        decreaseKey(size - 1, key);
+    }
+
+    private void decreaseKey(int i, int key) {
+        ls.set(i, key);
+        while (i > 0 && ls.get(parent(i)) > ls.get(i)) {
+            int tmp = ls.get(parent(i));
+            ls.set(parent(i), ls.get(i));
+            ls.set(i, tmp);
+            i = parent(i);
+        }
+    }
+
+    int left(int i) {
+        return 2 * i;
+    }
+
+    int right(int i) {
+        return 2 * i + 1;
+    }
+
+    int parent(int i) {
+        return i / 2;
+    }
+}
