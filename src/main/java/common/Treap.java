8e0f4b10e0e1e451ece07d56fa02359aad62e20a
Yogesh <ybhosale12@gmail.com>
2018-05-10 11:48:50 +0530

    1. Bug fixing phase

--- src/main/java/common/Treap.java
+++ src/main/java/common/Treap.java
@@ -28,7 +28,6 @@ public class Treap {
                 node.parent = cur;
             }
         } else {
-            node.rank -= cur.rank;
             if (cur.right != null) {
                 insert(cur.right, node);
             } else {
@@ -45,9 +44,9 @@ public class Treap {
         node.w = Integer.MAX_VALUE;
         insert(root, node);
         printDel(root);
-        maintainMaxHeap(node, true);
+        maintainMaxHeap(node, false);
         root = node;
-//        printDel(root);
+        printDel(root);
         return new TreapNode[]{node.left, node.right};
     }
 
@@ -86,11 +85,9 @@ public class Treap {
             if (nodeParent.left == node)
                 nodeParent.left = newParent;
             else nodeParent.right = newParent;
-        node.left = newParent.right;
-        newParent.right = node;
-        node.parent = newParent;
+        assignLeftChild(node, newParent.right);
+        assignRightChild(newParent, node);
         newParent.parent = nodeParent;
-        node.rank = node.left == null ? 1 : node.left.rank + 1;
     }
 
     private void leftRotate(TreapNode node) {
@@ -100,33 +97,26 @@ public class Treap {
             if (nodeParent.left == node)
                 nodeParent.left = newParent;
             else nodeParent.right = newParent;
-        node.right = newParent.left;
-        newParent.left = node;
-        newParent.rank += node.rank;
-        node.parent = newParent;
+        assignRightChild(node, newParent.left);
+        assignLeftChild(newParent, node);
         newParent.parent = nodeParent;
     }
 
-    public static void main(String[] args) {
-        int[] ar = {1, 2, 3};
-        Treap treap = new Treap();
-        for (int i = 0; i < ar.length; i++) {
-            int rank = i + 1;
-            int data = ar[i];
-            TreapNode node = new TreapNode();
-            node.data = data;
-            node.rank = rank;
-            node.w = rank % 2 == 1 ? rank * 2 : rank;
-            treap.insert(node);
-            treap.printDel(treap.root);
+    private static void assignLeftChild(TreapNode node, TreapNode child) {
+        if (node != null) {
+            node.left = child;
+            if (child != null) {
+                child.parent = node;
+            }
         }
+    }
 
-//        treap.printDel(treap.root);
-
-//        TreapNode[] sp = treap.split(2);
-
-//        treap.printDel(sp[0]);
-//        treap.printDel(sp[1]);
+    private static void assignRightChild(TreapNode node, TreapNode child) {
+        if (node != null) {
+            node.right = child;
+            if (child != null)
+                child.parent = node;
+        }
     }
 
     private void printDel(TreapNode node) {
@@ -142,5 +132,25 @@ public class Treap {
                 + " -> " + new Gson().toJson(node.parent));
         printTree(node.left);
         printTree(node.right);
+    }
+
+    public static void main(String[] args) {
+        int[] ar = {1, 2, 3};
+        Treap treap = new Treap();
+        for (int i = 0; i < ar.length; i++) {
+            int rank = i + 1;
+            int data = ar[i];
+            TreapNode node = new TreapNode();
+            node.data = data;
+            node.rank = rank;
+            node.w = rank % 2 == 1 ? rank * 2 : rank;
+            treap.insert(node);
+        }
+        treap.printDel(treap.root);
+
+        TreapNode[] sp = treap.split(2);
+
+        treap.printDel(sp[0]);
+        treap.printDel(sp[1]);
     }
 }
