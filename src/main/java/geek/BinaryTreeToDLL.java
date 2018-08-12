daf67e86f450c2cf37c6ee9a4d0c50925f7ac206
Yogesh <ybhosale12@gmail.com>
2018-08-12 14:32:42 +0530

    1. Binary tree to doubly linked list

---
+++ src/main/java/geek/BinaryTreeToDLL.java
@@ -0,0 +1,48 @@
+package geek;
+
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class DLLNode {
+    Node head;
+    Node tail;
+}
+
+public class BinaryTreeToDLL {
+    Node head;
+
+    Node BToDLL(Node root) {
+        DLLNode dll = convert(root);
+        head = dll.head;
+        return dll.head;
+    }
+
+    private DLLNode convert(Node root) {
+        if (root == null)
+            return null;
+        DLLNode left = convert(root.left);
+        DLLNode right = convert(root.right);
+        DLLNode res = new DLLNode();
+        if (left != null) {
+            res.head = left.head;
+            joinNodes(left.tail, root);
+        } else res.head = root;
+        if (right != null) {
+            res.tail = right.tail;
+            joinNodes(root, right.head);
+        } else res.tail = root;
+        return res;
+    }
+
+    private void joinNodes(Node node1, Node node2) {
+        node1.right = node2;
+        node2.left = node1;
+    }
+}
